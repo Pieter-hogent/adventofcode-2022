fe997e09fbc1a314a82db233b622dc0d3eb7b2b6
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2022-12-05 20:46:33 +0100

    day4

---
+++ code/4.2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const { resolve } = require('path');
+
+const overlap = ({min: minf, max: maxf}, {min: mins, max: maxs}) => {
+	return minf >= mins && minf <= maxs || maxf >= mins && maxf <= maxs ||
+	mins >= minf && mins <= maxf || maxs >= minf && maxs <= maxf;
+}
+
+const contained = ({min: minf, max: maxf}, {min: mins, max: maxs}) => {
+	return (minf >= mins && minf <= maxs && maxf >= mins && maxf <= maxs) ||
+	(mins >= minf && mins <= maxf && maxs >= minf && maxs <= maxf) ;
+}
+
+function solve(filename) {
+	let data = fs
+		.readFileSync(resolve(process.cwd(), filename), 'utf8')
+		.trim()
+		.split('\n').map(x => x.split(','));
+	
+		data = data.map(([first, second]) => {
+			const [a, b] = first.split('-');
+			const [c, d] = second.split('-');
+			return [{min:Number(a), max: Number(b)}, {min:Number(c), max: Number(d)}];
+		});
+
+		const pairs = data.reduce( (total, [first, second]) => total += overlap(first, second), 0);
+
+	return pairs;
+}
+
+console.log(solve('data/input4.1.txt'));
